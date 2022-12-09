30ecb0a5eefa747573f65a8694e6c37188651eda
Roman Zamolotov <rzamolotov@gmail.com>
2022-12-09 14:15:38 +0300

    Initial Commit

---
+++ TestAppMarketplace/TestAppMarketplaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestAppMarketplaceApp.swift
+//  TestAppMarketplace
+//
+//  Created by Роман Замолотов on 09.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct TestAppMarketplaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
